18235b1f4753d49412daa4ebaf428c8fa995af24
DemchaAV <demchaav@gmail.com>
2025-07-04 09:09:50 +0100

    *

--- src/main/java/com/demcha/spring_cart_api/repositories/OrderItemRepository.java
+++ src/main/java/com/demcha/spring_cart_api/repositories/OrderItemRepository.java
@@ -1,4 +1,4 @@
-package com.demcha.spring_cart_api.controlers;
+package com.demcha.spring_cart_api.repositories;
 
 import com.demcha.spring_cart_api.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
